0f537bddf9964bbbd2a87a5cb9c9bc733f333615
Jenndoza <j.mendo1997@gmail.com>
2022-06-17 22:42:49 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,7 +11,33 @@ project 1 - A Random Quote Generator
  * `quotes` array abc 
 ***/
 
-jennifer
+array ()[
+{
+  quote: “There is no such thing as easy, easy does not exist.”,
+  source: 'Anonymous',
+},
+{
+  quote: “Life is like a box of chocolates, you never know what you’re going to get.”,
+  source: 'Forrest',
+  citation: 'Forrest Gump',
+},
+{
+  quote: “I wanna be remembered as someone who broke all the norms.”,
+  source: 'Karol G',
+},
+{
+  quote: “It always seems impossible until it’s done.”,
+  source: 'Nelson Mandela',
+  year: '2001',
+},
+{
+  quote: “If you can make it through the night, there’s a brighter day.”,
+  source: 'Tupac Shakur',
+}
+
+console.log()
+
+];
 
 /***
  * `getRandomQuote` function
