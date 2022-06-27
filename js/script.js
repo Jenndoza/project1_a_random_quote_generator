f795ea21a86a460f53519b0235fbc1d877251206
Jenndoza <j.mendo1997@gmail.com>
2022-06-27 10:10:14 -0500

    Fixed citation

--- js/script.js
+++ js/script.js
@@ -11,45 +11,69 @@ project 1 - A Random Quote Generator
  * `quotes` array abc 
 ***/
 
-array ()[
+let quotes = 
+[
 {
-  quote: “There is no such thing as easy, easy does not exist.”,
+  quote: "There is no such thing as easy, easy does not exist.",
   source: 'Anonymous',
 },
 {
-  quote: “Life is like a box of chocolates, you never know what you’re going to get.”,
+  quote: "Life is like a box of chocolates, you never know what you're going to get.",
   source: 'Forrest',
   citation: 'Forrest Gump',
 },
 {
-  quote: “I wanna be remembered as someone who broke all the norms.”,
+  quote: "I wanna be remembered as someone who broke all the norms.",
   source: 'Karol G',
 },
 {
-  quote: “It always seems impossible until it’s done.”,
+  quote: "It always seems impossible until it's done.",
   source: 'Nelson Mandela',
   year: '2001',
 },
 {
-  quote: “If you can make it through the night, there’s a brighter day.”,
+  quote: "If you can make it through the night, there's a brighter day.",
   source: 'Tupac Shakur',
 }
-
-console.log()
-
 ];
+
+console.log(quotes)
 
 /***
  * `getRandomQuote` function
 ***/
 
+function getRandomQuote(){
+  const randomNumber = Math.floor(Math.random()*quotes.length);
+  return quotes [randomNumber];
+}
 
+getRandomQuote();
+//console.log(getRandomQuote)
 
 /***
  * `printQuote` function
 ***/
 
+function printQuote(){
+  let randomQuote = getRandomQuote();
 
+  let html= `<p class="quote" > ${randomQuote.quote} </p>
+              <p class="source"> ${randomQuote.source} `
+
+  if (randomQuote.citation) {
+    html+=`<span class="citation" > ${randomQuote.citation} </span>`
+  };
+  
+  if (randomQuote.year) {
+    html +=`<span class="year" > ${randomQuote.year} </span>`
+  };
+
+  html+= `</p>`;
+
+  document.getElementById("quote-box").innerHTML = html;
+  
+}
 
 /***
  * click event listener for the print quote button
